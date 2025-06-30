760ec916a9f91452654f072af192e45826d5f269
Evie Jones <147057981+eviejones@users.noreply.github.com>
2025-06-30 12:26:37 +0100

    feat: make main prettier

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+"""Loads the list of urls and downloads data both serially and in parallel. The combines this metadata into a single file."""
 from extract_videos import load_txt
 from parallel_processing import download_parallel
 from serial_processing import download_serial
-from extract_metadata import extract_metadata
-youtube_urls = load_txt("video_urls.txt")
+from combine_metadata import combine_metadata
 
-download_serial(youtube_urls)
-download_parallel(youtube_urls)
-extract_metadata("audio_output")
+if __name__ == "__main__":
+    youtube_urls = load_txt("video_urls.txt")
+    download_serial(youtube_urls)
+    download_parallel(youtube_urls)
+    combine_metadata("audio_output")
